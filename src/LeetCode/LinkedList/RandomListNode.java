f9784d3928a38827fe55b662dd2f71d7b8027466
ggorantla <ggorantl@eng.ucsd.edu>
2015-02-27 01:58:05 -0800

    Adding Random ListNode

---
+++ src/LeetCode/LinkedList/RandomListNode.java
@@ -0,0 +1,16 @@
+package LeetCode.LinkedList;
+
+/**
+ * Created by ggorantla on 2/26/2015.
+ */
+public class RandomListNode {
+    int val;
+    RandomListNode next;
+    RandomListNode random;
+    RandomListNode(int x){
+        val = x;
+        next = null;
+        random = null;
+    }
+            
+}
